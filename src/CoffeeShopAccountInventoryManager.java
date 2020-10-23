a241d1c0562be74faee7d0eb7890754d0f25e088
mosguinz <mos.guinz@gmail.com>
2020-10-23 14:06:21 -0700

    Add header

--- src/CoffeeShopAccountInventoryManager.java
+++ src/CoffeeShopAccountInventoryManager.java
@@ -1,3 +1,13 @@
+/*
+ * Assignment 07
+ * Description: Coffee shop inventory manager - refactored
+ * Name: Kullathon "Mos" Sitthisarnwattanachai
+ * Teammate: Amber Hartigan
+ * ID: 921425216
+ * Class: CSC 210-03
+ * Semester: Fall 2020
+ */
+
 import java.util.Arrays;
 import java.util.Scanner;
 
